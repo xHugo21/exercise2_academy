61f77087ba22653dfc9e9a910ef6f6541a166663
xHugo21 <hugog@hugog.local>
2023-03-09 11:31:23 +0100

    Added more tags to character cards

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,8 +3,10 @@
     <article class="character">
       	<img class="character__image" v-bind:alt="'character image of'+character.name" v-bind:src="character.image">
       	<div class="character__description">
-			<p class="character__name">{{ character.name }}</p>
-        	<div v-bind:class="['character__status','character__status--'+character.status.toLowerCase()]">{{ character.status }}</div>
+			<div class="character__name">{{ character.name }}</div>
+        	<div v-bind:class="['character__div','character__status--'+character.status.toLowerCase()]">{{ character.status }}</div>
+        	<div v-bind:class="['character__div','character__species--'+character.species.toLowerCase()]">{{ character.species }}</div>
+        	<div v-bind:class="['character__div','character__gender--'+character.gender.toLowerCase()]">{{ character.gender }}</div>
       	</div>
     </article>
 </template>
@@ -29,16 +31,18 @@
 		align-items: center;
 	}
 
-	.character__name{
+	/*.character__name{
 		padding: 2%;
 		border-radius:15px;
 		border: 2px solid purple;
-	}
+	}*/
 
-	.character__status {
+	.character__div {
 		padding: 2%;
 		border-radius:15px;
 		border: 2px solid purple;
+		margin-top: 3%;
+		margin-bottom: 3%;
 	}
     .character__status--alive {
     	background-color: #00ff00;
@@ -50,6 +54,42 @@
 
 	.character__status--unknown {
 		background-color: grey;
+	}
+
+	.character__gender--male{
+		background-color: aqua;
+	}
+
+	.character__gender--female{
+		background-color: pink;
+	}
+
+	.character__gender--unknown{
+		background-color: grey;
+	}
+
+	.character__species--human{
+		background-color: #dbb798;
+	}
+
+	.character__species--alien{
+		background-color: #b7d7d7;
+	}
+
+	.character__species--humanoid{
+		background-color: #cb6e1d;
+	}
+
+	.character__species--cronenberg{
+		background-color:darkkhaki;
+	}
+
+	.character__species--unknown{
+		background-color:grey;
+	}
+
+	.character__species--poopybutthole{
+		background-color: #1f5104;
 	}
 
 </style>
