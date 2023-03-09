2cf5e19f9701c1d4ed3610ec6f8a4726183b7a66
xHugo21 <hugog@hugog.local>
2023-03-09 18:17:12 +0100

    Added transitions

--- src/components/FiltersGrid.vue
+++ src/components/FiltersGrid.vue
@@ -1,9 +1,11 @@
 <!--Component that defines the section where a filter form is placed-->
 <template>
     <ul>
+		<TransitionGroup name="animationfilters"> 
     	<li v-for="filter in filters" v-bind:key="filter">
         	<slot :filter="filter"></slot>
       	</li>
+		</TransitionGroup>
     </ul>
 </template>
   
@@ -19,5 +21,15 @@
 	ul {
 		list-style: none;
 		padding: 0;
+	}
+
+	.animationfilters-enter-active,
+	.animationfilters-leave-active {
+		transition: all 0.8s ease;
+	}
+	.animationfilters-enter-from,
+	.animationfilters-leave-to {
+		opacity: 0;
+		transform: translateX(30px);
 	}
 </style>
